086c589a8c546e1971e32b529540cf04d0a54439
unknown <css105680@HDC0006893.ad.csscorp.com>
2014-12-23 19:09:07 +0530

    updated

--- myfirst/src/Controller/FirstPageController.php
+++ myfirst/src/Controller/FirstPageController.php
@@ -30,26 +30,40 @@ class FirstPageController extends ControllerBase {
     _drupal_add_css(drupal_get_path('module', 'myfirst') . '/cleditor/css/jquery.cleditor.css');
     _drupal_add_js(drupal_get_path('module', 'myfirst') . '/cleditor/js/jquery.cleditor.min.js');
     //_drupal_add_js(drupal_get_path('module', 'myfirst') . '/js/myfirst.js');    
-    
+
     $form['drop_cnt'] = array(
-      '#markup' => '<script src="'. drupal_get_path('module', 'myfirst') . '/js/myfirst.js' .'"></script><div id="text-editor">',
-    ); 
+      '#markup' => '<script src="' . drupal_get_path('module', 'myfirst') . '/js/myfirst.js' . '"></script><div id="text-editor">',
+    );
     $form['drop_cnt_inner'] = array(
       '#markup' => '<div style="width:300px; height:150px; border: 1px solid red"><div class="drop-target no-cnt" style="height:10px"></div>'
       . '<div>Drop content</div>'
       . '<div class="drop-target no-cnt" style="height:10px"></div></div>',
-    );     
+    );
     $form['draggable_div'] = array(
       '#markup' => '<div draggable="true" id="drag-cnt">Text</div>'
-    );    
+    );
     $form['click_test'] = array(
-       '#markup' => '<div class="click-cnt">
+      '#markup' => '<div class="click-cnt">
       <div class="click-test">click</div>
       </div><div class="addclick">Add Click</div>',
     );
-     return $form;
+    return $form;
   }
 
+  public function firstpage_create_tables() {
+    if (db_table_exists('template_details') == -1) {
+      $template_details = array(
+        'id' => array('type' => 'serial'),
+        'template_display_name' => array('type' => 'varchar', 'length' => 150),
+        'template_image' => array('type' => 'text'),
+        'template_file' => array('type' => 'text'),
+      );
+      db_create_table('template_details');
+    }
+  }
+  
+  public function firstpage_display_template() {
+    
+  }
 }
-
 ?>
